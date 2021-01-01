f141503e010bea9ad6d5667ae95742f01828dfee
Dario <jeffrey.ayala2040@gmail.com>
2020-12-31 23:16:10 -0400

    add interactivity: Subclass State

--- lib/main.dart
+++ lib/main.dart
@@ -118,8 +118,40 @@ class FavoriteWidget extends StatefulWidget {
 }
 
 class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorite = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorite) {
+        _favoriteCount -= 1;
+        _isFavorite = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorite = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+              icon: _isFavorite ? Icon(Icons.star) : Icon(Icons.star_border),
+              color: Colors.red[500],
+              onPressed: _toggleFavorite),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
   }
 }
